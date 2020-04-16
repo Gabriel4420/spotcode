c635aedcc369534579ebd560fa0e72e17a16bd75
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2020-04-16 01:25:14 -0300

    updates dashboard.rb ,index.json e routes.rb

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -1,13 +1,24 @@
 class Api::V1::DashboardController < ApplicationController
     
     def index 
+        load_recent_heard
+        load_recommendations
     end 
 
     private 
 
     def load_recent_heard
-        @recent_albums = current_user.recently_heards.order('created_at DESC').limit(4).map(&:album)
+        @recent_albums = current_user.recently_heard.order('created_at DESC').limit(4).map(&:album)
     end
 
+    def load_recommendations
+        heard_categories = @recent_albums.map(&:category)
+        if heard_categories.present?
+            @recommend_albums = Album.join(:category, :songs).where(category: heard_categories)
+            .order('songs.played_count').select('distinct algums.*').limit(12)
+        else
+            @recommend_albums = Album.all.sample(12)
+    end
 
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get 'home/index'
   root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do  
+        resources :dashboard, only: :index
+    end 
+  end
 end
