dd4fa59bdedeb06f4213d2ced14a3a162b543ec1
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2020-04-24 19:11:02 -0300

    bug fixed missing end at the end of the file

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
         resources :dashboard, only: :index
         resources :categories, only: %i[index show]
         resources :search, only: :index
-        resources :albums, only: :show
-        
+        resources :albums, only: :show do
+          resources :recently_heards, only: :create
+        end
     end 
   end
 end
